c5c853bfdd2678036e3466ecbb4d53440d746e2d
Nitrofski <vincent.fortin.94@gmail.com>
2019-04-02 23:52:48 -0400

    Lots of cleanup ahead

--- server.mjs
+++ server.mjs
@@ -11,50 +11,103 @@ discordClient.on('ready', () => {
   console.log(`Logged in as ${discordClient.user.tag}!`)
 })
 
-const _processCodeBlocks = async (msg, regex) => {
-  let match
-  let matched = false
-  while ((match = regex.exec(msg.content)) !== null) {
-    await _processLilyInput(msg, match.groups.data)
-    matched = true
+/**
+ * @param msg {Discord.Message}
+ * @param regex {RegExp}
+ */
+async function _processCodeBlocks (msg, regex) {
+  let typing = false
+  let attachments = []
+  for (let i = 0, match; (match = regex.exec(msg.content)) !== null; ++i) {
+    if (i === 0) {
+      typing = true
+      msg.channel.startTyping()
+    }
+
+    console.log(`Processing Lily request from @${msg.author.tag}`)
+    const image = await _processLilyInput(msg, match.groups.data)
+    if (image) {
+      attachments.push(new Discord.Attachment(image, `lily${(i + 1).toString().padStart(2, '0')}.png`))
+    }
   }
 
-  return matched
+  try {
+    if (attachments.length !== 0) {
+      if (attachments.length === 1) {
+        await msg.channel.send(new Discord.RichEmbed()
+          .attachFile(attachments[0])
+          .setImage('attachment://lily01.png')
+          .setColor([100, 125, 100]))
+      } else {
+        await msg.channel.send({ files: attachments })
+      }
+      await msg.react('🌺')
+      return true
+    }
+  } catch (e) {
+    await msg.reply('Fatal error when sending reply...')
+    console.error(e)
+  } finally {
+    if (typing) {
+      msg.channel.stopTyping()
+    }
+  }
+
+  return false
 }
 
-const _processLilyInput = async (msg, data) => {
-  console.log(`Received Lily request from @${msg.author.tag}`)
-  msg.channel.startTyping()
-
+/**
+ * @param msg {Discord.Message}
+ * @param data {String}
+ */
+async function _processAsSingleInput (msg, data) {
   try {
-    const image = await Pond.fish(data)
-    await msg.channel.send(new Discord.RichEmbed()
-      .attachFile({ attachment: image, name: 'lily.png' })
-      .setImage('attachment://lily.png')
-      .setColor([100, 125, 100]))
+    msg.channel.startTyping()
+    console.log(`Processing Lily request from @${msg.author.tag}`)
+    const image = await _processLilyInput(msg, data)
+    if (image) {
+      await msg.channel.send(new Discord.RichEmbed()
+        .attachFile({ attachment: image, name: 'lily.png' })
+        .setImage('attachment://lily.png')
+        .setColor([100, 125, 100]))
+      await msg.react('🌺')
+    }
+    msg.channel.stopTyping()
+  } catch (e) {
+    await msg.reply('Fatal error when sending reply...')
+    console.error(e)
+  } finally {
+    msg.channel.stopTyping()
+  }
+}
+
+/**
+ * @param msg {Discord.Message}
+ * @param data {String}
+ */
+async function _processLilyInput (msg, data) {
+  try {
+    return await Pond.fish(data)
     // await reply.react('🗑')
-    await msg.react('🌺')
   } catch (e) {
     console.error(e)
-    let reply = `<@${msg.author.id}>, there was an error when parsing the input:\`\`\`\n${
+    let reply = `<@${msg.author.id}>, LilyPond could not parse your input:\`\`\`\n${
       e.message.replace(/```/g, '<triple-backtick>')}\`\`\``
     if (reply.length > 2000) {
       let ellipsis = '[...]'
       reply = reply.substring(0, reply.lastIndexOf('\n', 1997 - ellipsis.length)) + ellipsis + '```'
     }
     await msg.channel.send(reply)
-  } finally {
-    msg.channel.stopTyping()
   }
 }
 
 discordClient.on('message', async msg => {
   if (msg.content.startsWith('\\lily')) {
     // First, look for code blocks
-    const matched = await _processCodeBlocks(msg, /```(\w+\n)?\n*(?<data>.+?)\n*```/gms)
+    const processed = await _processCodeBlocks(msg, /```(\w+\n)?\n*(?<data>.+?)\n*```/gms)
     // If no code blocks was found, treat the whole message as lily input.
-    if (!matched) {
-      return _processLilyInput(msg, msg.content.substr('\\lily'.length))
+    if (!processed) {
+      return _processAsSingleInput(msg, msg.content.substr('\\lily'.length))
     }
   } else {
     return _processCodeBlocks(msg, /```lily\n+(?<data>.+?)\n*```/gms)
--- src/pond.mjs
+++ src/pond.mjs
@@ -1,60 +1,110 @@
 import Settings from './../settings.mjs'
-import util from 'util'
+
 import childProcess from 'child_process'
 import fs from 'fs-extra'
+import escapeRegExp from 'lodash.escaperegexp'
 import path from 'path'
-import { tmpdir } from 'os'
+import util from 'util'
+
+import { tmpdir, EOL } from 'os'
 
 const execFile = util.promisify(childProcess.execFile)
 
-const _createInputData = data => {
+function _createInputData (data) {
   data = data.trim()
   if (!data.startsWith('\\version')) {
-    data =
-      '\\version "2.18.2"\n' +
-      '\\paper{\n' +
-      '  oddHeaderMarkup  = ##f\n' +
-      '  evenHeaderMarkup = ##f\n' +
-      '  oddFooterMarkup  = ##f\n' +
-      '  evenFooterMarkup = ##f\n' +
-      '}\n' +
-      '\\layout {\n' +
-      '  line-width  = 400\\pt\n' +
-      '  indent      = #0\n' +
-      '  ragged-last = ##t\n' +
-      '  \\context {\n' +
-      '    \\Score\n' +
-      '    \\omit BarNumber\n' +
-      '  }\n' +
-      '  \\context {\n' +
-      '    \\Staff\n' +
-      '    \\numericTimeSignature\n' +
-      '  }\n' +
-      '}\n\n' +
-      data
+    return {
+      data:
+        '\\version "2.18.2"\n' +
+        '\\paper{\n' +
+        '  oddHeaderMarkup  = ##f\n' +
+        '  evenHeaderMarkup = ##f\n' +
+        '  oddFooterMarkup  = ##f\n' +
+        '  evenFooterMarkup = ##f\n' +
+        '}\n' +
+        '\\layout {\n' +
+        '  line-width  = 400\\pt\n' +
+        '  indent      = #0\n' +
+        '  ragged-last = ##t\n' +
+        '  \\context {\n' +
+        '    \\Score\n' +
+        '    \\omit BarNumber\n' +
+        '  }\n' +
+        '  \\context {\n' +
+        '    \\Staff\n' +
+        '    \\numericTimeSignature\n' +
+        '  }\n' +
+        '}\n\n' +
+        data,
+      addedLinesCount: 21
+    }
   }
-  return data
+
+  return { data: data, addedLinesCount: 0 }
+}
+
+/**
+ * @param stderr {String}
+ * @param inputFilename {String}
+ */
+function _sanitizeLilypondLog (stderr, inputFilename, addedLinesCount) {
+  // The first 2 lines are always "Processing `in.ly`" and "Parsing..." respectively...
+  // The last 2 lines should always be "fatal error: failed files: ..." and an empty line...
+  let lines = stderr.split(EOL).slice(2, -2)
+
+  const errorDescriptionRegex = new RegExp(
+    `^${escapeRegExp(inputFilename.replace(/\\/g, '/'))}:(?<line>\\d+):(?<col>\\d+):(?<msg>.*)`)
+
+  for (let i = 0; i < lines.length; ++i) {
+    let match
+    if ((match = lines[i].match(errorDescriptionRegex))) {
+      lines[i] = `<input>:${match.groups.line - addedLinesCount}:${match.groups.col}:${match.groups.msg}`
+    }
+  }
+
+  return lines.join(EOL)
 }
 
 export default {
-  fish: async data => {
+  /**
+   * @param data {String}
+   */
+  fish: async function (data) {
     let tmp
     try {
-      tmp = await fs.mkdtemp(path.join(tmpdir(), 'lily-'))
-      await fs.writeFile(path.join(tmp, 'in.ly'), _createInputData(data))
+      let inputFilename
+      let addedLinesCount
+      try {
+        tmp = await fs.mkdtemp(path.join(tmpdir(), 'lily-'))
+        inputFilename = path.join(tmp, 'in.ly')
 
-      // Lilypond has a "jail" mode, which would be very useful for security on an actual server.
-      await execFile(Settings.lilypondPath,
-        ['-dbackend=eps', '-dno-gs-load-fonts', '-dinclude-eps-fonts', '--png', '-dresolution=300', '--output=out', path.join(tmp, 'in.ly')],
-        { cwd: tmp })
+        const inputData = _createInputData(data)
+        addedLinesCount = inputData.addedLinesCount
+        await fs.writeFile(inputFilename, inputData.data)
+      } catch (e) {
+        throw new Error(`Internal server error: ${e.message}`)
+      }
+
+      try {
+        // Lilypond has a "jail" mode, which would be very useful for security on an actual server.
+        await execFile(Settings.lilypondPath,
+          ['-dbackend=eps', '-dno-gs-load-fonts', '-dinclude-eps-fonts', '--png', '-dresolution=300', '--output=out', inputFilename],
+          { cwd: tmp })
+      } catch (e) {
+        throw new Error(_sanitizeLilypondLog(e.stderr, inputFilename, addedLinesCount))
+      }
 
       const pngFile = path.join(tmp, 'out.png')
       if (!await fs.exists(pngFile)) {
         throw new Error('The given input generated no output.')
       }
 
-      // Clean up after ourselves. Load the image in memory, then delete the temp dir immediately.
-      return await fs.readFile(pngFile)
+      try {
+        // Clean up after ourselves. Load the image in memory, then delete the temp dir immediately.
+        return await fs.readFile(pngFile)
+      } catch (e) {
+        throw new Error(`Internal server error: The output image could not be read.`)
+      }
     } finally {
       if (tmp) {
         await fs.remove(tmp)
